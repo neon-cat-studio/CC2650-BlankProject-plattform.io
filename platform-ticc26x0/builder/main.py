e659d82e32861a1bcb754df93bc5ca0139285022
霓虹貓工作室 <neon.cat.studio.840202@gmail.com>
2025-01-26 00:15:59 +0800

    blank

---
+++ platform-ticc26x0/builder/main.py
@@ -0,0 +1,252 @@
+# Copyright 2014-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+    Builder for Texas Instruments
+    Tiva C Series ARM Cortex-M4 microcontrollers.
+"""
+
+import sys
+from platform import system
+from os.path import join
+
+from SCons.Script import (ARGUMENTS, COMMAND_LINE_TARGETS, AlwaysBuild,
+                          Builder, Default, DefaultEnvironment)
+
+env = DefaultEnvironment()
+platform = env.PioPlatform()
+board = env.BoardConfig()
+upload_protocol = env.subst("$UPLOAD_PROTOCOL")
+
+env.Replace(
+    AR="arm-none-eabi-ar",
+    AS="arm-none-eabi-as",
+    CC="arm-none-eabi-gcc",
+    CXX="arm-none-eabi-g++",
+    GDB="arm-none-eabi-gdb",
+    OBJCOPY="arm-none-eabi-objcopy",
+    RANLIB="arm-none-eabi-ranlib",
+    SIZETOOL="arm-none-eabi-size",
+
+    ARFLAGS=["rc"],
+
+    SIZEPROGREGEXP=r"^(?:\.text|\.data|\.rodata|\.text.align|\.ARM.exidx)\s+(\d+).*",
+    SIZEDATAREGEXP=r"^(?:\.data|\.bss|\.noinit)\s+(\d+).*",
+    SIZECHECKCMD="$SIZETOOL -A -d $SOURCES",
+    SIZEPRINTCMD='$SIZETOOL -B -d $SOURCES',
+
+    PROGSUFFIX=".elf"
+)
+
+# Allow user to override via pre:script
+if env.get("PROGNAME", "program") == "program":
+    env.Replace(PROGNAME="firmware")
+
+env.Append(
+    ASFLAGS=["-x", "assembler-with-cpp"],
+
+    CCFLAGS=[
+        "-O0",
+        "-g",
+        "-ffunction-sections",  # place each function in its own section
+        "-fdata-sections",
+        "-Wall",
+        "-march=armv7-m",
+        "-mthumb"
+    ],
+
+    CFLAGS=[
+        "-std=gnu11"
+    ],
+
+    CXXFLAGS=[
+        "-fno-rtti",
+        "-fno-exceptions",
+        "-fno-threadsafe-statics",
+        "-std=gnu++11",
+    ],
+
+    CPPDEFINES=[
+        ("F_CPU", "$BOARD_F_CPU")
+    ],
+
+    LINKFLAGS=[
+        "-O0",
+        "-march=armv7-m",
+        "-mthumb",
+        "-nostartfiles",
+        "-specs=nano.specs",
+        "-specs=rdimon.specs",
+        "-specs=nosys.specs"
+    ],
+
+    LIBS=["stdc++", "gcc", "c", "m", "nosys"],
+
+    BUILDERS=dict(
+        ElfToBin=Builder(
+            action=env.VerboseAction(" ".join([
+                "$OBJCOPY",
+                "-O",
+                "binary",
+                "$SOURCES",
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".bin"
+        ),
+        ElfToHex=Builder(
+            action=env.VerboseAction(" ".join([
+                "$OBJCOPY",
+                "-O",
+                "ihex",
+                "-R",
+                ".eeprom",
+                "$SOURCES",
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".hex"
+        )
+    )
+)
+
+if "BOARD" in env:
+    env.Append(
+        CCFLAGS=[
+            "-mcpu=%s" % board.get("build.cpu")
+        ],
+        LINKFLAGS=[
+            "-mcpu=%s" % board.get("build.cpu")
+        ]
+    )
+
+# copy CCFLAGS to ASFLAGS (-x assembler-with-cpp mode)
+env.Append(ASFLAGS=env.get("CCFLAGS", [])[:])
+
+if "energia" in env.get("PIOFRAMEWORK", []):
+    sys.stderr.write(
+        "WARNING!!! Using of `framework = energia` in `platformio.ini` is "
+        "deprecated. Please replace with `framework = arduino`.\n")
+    env.Replace(PIOFRAMEWORK=["arduino"])
+
+#
+# Target: Build executable and linkable firmware
+#
+
+target_elf = None
+if "nobuild" in COMMAND_LINE_TARGETS:
+    target_elf = join("$BUILD_DIR", "${PROGNAME}.elf")
+    target_firm = join("$BUILD_DIR", "${PROGNAME}.bin")
+else:
+    target_elf = env.BuildProgram()
+    target_firm = env.ElfToBin(join("$BUILD_DIR", "${PROGNAME}"), target_elf)
+
+AlwaysBuild(env.Alias("nobuild", target_firm))
+target_buildprog = env.Alias("buildprog", target_firm, target_firm)
+
+#
+# Target: Print binary size
+#
+
+target_size = env.Alias(
+    "size", target_elf,
+    env.VerboseAction("$SIZEPRINTCMD", "Calculating size $SOURCE"))
+AlwaysBuild(target_size)
+
+#
+# Target: Upload firmware
+#
+
+debug_tools = board.get("debug.tools", {})
+upload_actions = []
+
+if upload_protocol.startswith("jlink"):
+
+    def _jlink_cmd_script(env, source):
+        build_dir = env.subst("$BUILD_DIR")
+        if not isdir(build_dir):
+            makedirs(build_dir)
+        script_path = join(build_dir, "upload.jlink")
+        commands = [
+            "h",
+            "loadbin %s, %s" % (source, env.BoardConfig().get(
+                "upload.offset_address", "0x0")),
+            "r",
+            "q"
+        ]
+        with open(script_path, "w") as fp:
+            fp.write("\n".join(commands))
+        return script_path
+
+    env.Replace(
+        __jlink_cmd_script=_jlink_cmd_script,
+        UPLOADER="JLink.exe" if system() == "Windows" else "JLinkExe",
+        UPLOADERFLAGS=[
+            "-device", env.BoardConfig().get("debug", {}).get("jlink_device"),
+            "-speed", "4000",
+            "-if", ("jtag" if upload_protocol == "jlink" else "cjtag"),
+            "-autoconnect", "1"
+        ],
+        UPLOADCMD='$UPLOADER $UPLOADERFLAGS -CommanderScript "${__jlink_cmd_script(__env__, SOURCE)}"'
+    )
+    upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
+
+elif upload_protocol.startswith("dslite"):
+
+    env.Replace(
+        UPLOADERPATH=platform.get_package_dir("tool-dslite") or "",
+        UPLOADERNAME="DSLite.exe" if system() == "Windows" else "DSLite",
+	    UPLOADER=join("$UPLOADERPATH", "DebugServer", "bin", "$UPLOADERNAME"),
+        UPLOADERCFG=join("$UPLOADERPATH",env.BoardConfig().get("build.variant") + ".ccxml"),
+        UPLOADERFLAGS=["load", "-c", "$UPLOADERCFG", "-f", "$SOURCE"],
+        UPLOADCMD='"$UPLOADER" $UPLOADERFLAGS')
+
+    upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
+
+elif upload_protocol in debug_tools:
+
+	openocd_args = [
+	    "-d%d" % (2 if int(ARGUMENTS.get("PIOVERBOSE", 0)) else 1)
+	]
+	#openocd_args.extend(board.get("debug.tools.ti-xds110.server.arguments", []))
+	openocd_args.extend(
+		debug_tools.get(upload_protocol).get("server").get("arguments", []))
+	openocd_args.extend([
+	    "-c", "program {$SOURCE} %s verify reset; shutdown;" %
+	    board.get("upload.offset_address", "")
+	])
+	openocd_args = [
+	    f.replace("$PACKAGE_DIR",
+	              platform.get_package_dir("tool-openocd") or "")
+	    for f in openocd_args
+	]
+	env.Replace(
+	    UPLOADER="openocd",
+	    UPLOADERFLAGS=openocd_args,
+	    UPLOADCMD="$UPLOADER $UPLOADERFLAGS")
+
+	upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
+
+# custom upload tool
+elif upload_protocol == "custom":
+    upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
+
+else:
+    sys.stderr.write("Warning! Unknown upload protocol %s\n" % upload_protocol)
+
+AlwaysBuild(env.Alias("upload", target_elf, upload_actions))
+
+#
+# Target: Default targets
+#
+
+Default([target_buildprog, target_size])
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+void hardware_init(void) {
+}
+
+int main(void) {
+    hardware_init();
+
+    while (1) {
+    }
+
+    return 0;
+}
